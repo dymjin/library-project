e1301ae4995492cabcb51df473d49a5738327f84
dymjin <dymjin@gmail.com>
2023-11-01 11:39:09 +0200

    Move project files into repo folder* *forgot to do this a while ago, now it's not nested in another folder

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const container = document.createElement('div');
 const bookDialog = document.getElementById('book-dialog');
 const openDialog = document.getElementById('open-dialog');
+const confirmBtn = document.getElementById('confirm-btn');
 const dialogContainer = document.getElementById('dialog-container');
+const pageCount = document.getElementById('pages');
+const bookAuthor = document.getElementById('author');
+const bookTitle = document.getElementById('book-title');
+
+
 container.classList.add('container');
 document.body.appendChild(container);
+
 const myLibrary = [];
 
 openDialog.addEventListener('click', () => {
     bookDialog.showModal();
 });
+
+confirmBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    // addBookToLibrary();
+    console.log(pageCount.value);
+    bookDialog.close();
+})
 
 function Book(title, author, pages, hasRead) {
     this.title = title
@@ -33,18 +47,22 @@ function displayBooks() {
         let bookIcons = document.createElement('div');
         let favoriteIcon = document.createElement('span');
         let pagesIcon = document.createElement('div');
+
         title.textContent = `${books.title}`;
         author.textContent = `${books.author}`;
+        favoriteIcon.textContent = "star";
+        pagesIcon.innerHTML = `<span class="material-symbols-outlined">auto_stories</span>${books.pages}`;
+
         title.classList.add('book-title');
         author.classList.add('book-author');
         favoriteIcon.classList.add("material-symbols-outlined");
-        favoriteIcon.textContent = "star";
-        pagesIcon.innerHTML = `<span class="material-symbols-outlined">auto_stories</span>${books.pages}`;
         book.classList.add('book');
-        book.appendChild(title);
-        book.appendChild(author);
         bookContainer.classList.add('book-container');
         bookIcons.classList.add('book-icons');
+
+        book.appendChild(title);
+        book.appendChild(author);
+
         bookIcons.appendChild(favoriteIcon);
         bookIcons.appendChild(pagesIcon);
         bookContainer.appendChild(book);
